d71e22407555697c5fe63efba61aa1df634b5b29
suhailandproud <74553302+suhailandproud@users.noreply.github.com>
2020-12-21 13:24:06 +0530

    Todolist
    A todolist which gets task-to-do from a backend json from outer source and display it in our webpage. The task name will change for every minute. The name, task status and comments of user will be saved in an array created inside.

---
+++ Todolist/Todolist.js
@@ -0,0 +1,52 @@
+const TASKFETCH_URL = "https://jsonplaceholder.typicode.com/comments";
+let taskArray = [];
+const completeArray = [];
+let currentTask = 1;
+
+
+
+
+
+fetch(TASKFETCH_URL)
+.then(function(response) {
+    return response.json();
+})
+.then(function(data) {
+    for (i=0; i<data.length; i++) {
+        
+        taskArray.push(data[i].name);    
+};
+userDescription.innerText = taskArray[0];
+})
+.then(feed());
+
+
+const userDescription = document.querySelector('.userDescription');
+
+
+function feed() { setInterval (function() {
+
+    userDescription.innerText = taskArray[currentTask]; 
+    currentTask++;
+}, 60000)
+};
+
+let upload= document.querySelector('.upload');
+let userName= document.getElementById('userName');
+let checkbox = document.querySelector('.checkbox');
+let comments = document.getElementById('comments');
+
+upload.addEventListener('click', function() {
+    if (userName.value) {
+        completeArray.push({'name' : userName.value,
+                             'description': userDescription.innerText,
+                            'taskComplete': checkbox.checked,
+                            'comments' : comments.value}); 
+                                               
+    } else {
+        alert ("Make sure you answer everything");
+    };
+    userName.value = '';
+    console.log(completeArray);
+})
+
